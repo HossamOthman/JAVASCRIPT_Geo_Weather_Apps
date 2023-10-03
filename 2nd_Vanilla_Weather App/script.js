b84a50571f627067ecaf0953a37293e3d76bb8e2
Hossam Othman <rowheadrex@gmail.com>
2023-10-03 14:55:49 +0200

    corrected error behavior

--- 2nd_Vanilla_Weather App/script.js
+++ 2nd_Vanilla_Weather App/script.js
@@ -16,29 +16,32 @@ async function checkWeater(city) {
 
     if (response.status == 404) {
         document.querySelector('.error').style.display = 'block';
+        weatherDiv.style.display = 'none';
+    } else {
+
+        var data = await response.json();
+        document.querySelector('.error').style.display = 'none';
+            document.querySelector('.city').innerHTML = data.name;
+            document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°C';
+            document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
+            document.querySelector('.wind').innerHTML = data.wind.speed + 'km/h';
+
+            if (data.weather[0].main == 'Clear') {
+                weatherIcon.src = 'img/clear.png';
+            } else if (data.weather[0].main == 'Clouds') {
+                weatherIcon.src = 'img/clouds.png';
+            }  else if (data.weather[0].main == 'Mist') {
+                weatherIcon.src = 'img/mist.png';
+            } else if (data.weather[0].main == 'Rain') {
+                weatherIcon.src = 'img/rain.png';
+            } else if (data.weather[0].main == 'Snow') {
+                weatherIcon.src = 'img/snow.png';
+            } else if (data.weather[0].main == 'Drizzle') {
+                weatherIcon.src = 'img/drizzle.png';
+            }
+
+            weatherDiv.style.display = 'block';
     }
 
-    var data = await response.json();
-
-    console.log(data)
-    document.querySelector('.city').innerHTML = data.name;
-    document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°C';
-    document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
-    document.querySelector('.wind').innerHTML = data.wind.speed + 'km/h';
-
-    if (data.weather[0].main == 'Clear') {
-        weatherIcon.src = 'img/clear.png';
-    } else if (data.weather[0].main == 'Clouds') {
-        weatherIcon.src = 'img/clouds.png';
-    }  else if (data.weather[0].main == 'Mist') {
-        weatherIcon.src = 'img/mist.png';
-    } else if (data.weather[0].main == 'Rain') {
-        weatherIcon.src = 'img/rain.png';
-    } else if (data.weather[0].main == 'Snow') {
-        weatherIcon.src = 'img/snow.png';
-    } else if (data.weather[0].main == 'Drizzle') {
-        weatherIcon.src = 'img/drizzle.png';
-    }
-
-    weatherDiv.style.display = 'block';
+    
 }
