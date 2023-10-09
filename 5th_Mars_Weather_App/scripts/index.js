9cc7c43a8e0bfcda1764718fe7d4d91dface36a2
Hossam Othman <rowheadrex@gmail.com>
2023-10-09 16:46:18 +0200

    template set

---
+++ 5th_Mars_Weather_App/scripts/index.js
@@ -0,0 +1,8 @@
+const prevWeatherToggle = document.querySelector('.show-previous-weather');
+const prevWeather = document.querySelector('.previous-weather');
+
+prevWeatherToggle.addEventListener('click', () => {
+    prevWeather.classList.toggle('show-weather')
+
+
+})
