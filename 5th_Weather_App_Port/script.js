a8ec4ada903e2148009700d660f9e3a976564611
Hossam Othman <rowheadrex@gmail.com>
2023-10-10 13:41:15 +0200

    received Data from API

--- 5th_Weather_App_Port/script.js
+++ 5th_Weather_App_Port/script.js
@@ -1,7 +1,37 @@
 let appId = '';
-let units = 'imperial';
+let units = 'metric';
 let searchMethod;
 
+function getSearchMethod(searchTerm) {
+    if (searchTerm === 5 && Number.parseInt(searchTerm) + '' === searchTerm)
+         searchMethod = 'zip';
+    else 
+         searchMethod = 'q';
+}
+
+
+document.getElementById('searchBtn').addEventListener('click', () => {
+    let searchTerm = document.getElementById('searchInput').value;
+    if(searchTerm) {
+        searchWeather(searchTerm)
+    }
+})
+
+
+
 function searchWeather(searchTerm) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=${appId}`)
+    getSearchMethod(searchTerm);
+    fetch(`https://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&appid=${appId}&units=${units}`)
+    .then(result => {
+        return result.json()
+    })
+    .then(result => {
+        init(result)
+    })
+}
+
+
+
+function init(resultFromServer) {
+    console.log(resultFromServer)
 }
