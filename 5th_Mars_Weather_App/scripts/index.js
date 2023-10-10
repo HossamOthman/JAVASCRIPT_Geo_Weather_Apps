22277fef5c3fe4e03b1821fa9d5cde9d8f692660
Hossam Othman <rowheadrex@gmail.com>
2023-10-10 11:02:15 +0200

    remove mars app

--- 5th_Mars_Weather_App/scripts/index.js
+++
@@ -1,8 +0,0 @@
-const prevWeatherToggle = document.querySelector('.show-previous-weather');
-const prevWeather = document.querySelector('.previous-weather');
-
-prevWeatherToggle.addEventListener('click', () => {
-    prevWeather.classList.toggle('show-weather')
-
-
-})
