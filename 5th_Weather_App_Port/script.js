53dd0707579a72aa5553b1420ae8eeeb261401ca
Hossam Othman <rowheadrex@gmail.com>
2023-10-10 15:03:10 +0200

    weather elements filled in UI

--- 5th_Weather_App_Port/script.js
+++ 5th_Weather_App_Port/script.js
@@ -34,4 +34,46 @@ function searchWeather(searchTerm) {
 
 function init(resultFromServer) {
     console.log(resultFromServer)
+    switch (resultFromServer.weather[0].main) {
+        case 'Clear':
+            document.body.style.backgroundImage  = 'url("img/clear.jpeg")'
+            break;
+        case 'Clouds':
+            document.body.style.backgroundImage  = 'url("img/cloud.jpg")'
+            break;
+        case 'Rain':
+        case 'Drizzle':
+        case 'Mist':
+            document.body.style.backgroundImage  = 'url("img/rain.jpg")'
+            break;
+        case 'Storm':
+        case 'Thunderstorm':
+            document.body.style.backgroundImage  = 'url("img/storm.jpg")'
+            break;
+        case 'Snow':
+            document.body.style.backgroundImage  = 'url("img/snow.jpg")'
+            break;
+        default:
+            document.body.style.backgroundImage  = 'url("img/default.jpg")'
+            break;
+    }
+
+    let cityHeader = document.getElementById('cityHeader');
+    let temperature = document.getElementById('temperature');
+    let weatherDescriptionHeader = document.getElementById('weatherDescriptionHeader');
+    let documentIconImg = document.getElementById('documentIconImg');
+    let windSpeed = document.getElementById('windSpeed');
+    let humidity = document.getElementById('humidity');
+
+    documentIconImg.src = `https://openweathermap.org/img/wn/${resultFromServer.weather[0].icon}@2x.png`;
+    
+    let resultDescription = resultFromServer.weather[0].description;
+    weatherDescriptionHeader.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1)
+    
+    temperature.innerHTML = Math.floor(resultFromServer.main.temp) + '&deg;';
+    windSpeed.innerHTML =  Math.floor(resultFromServer.wind.speed) + 'm/s';
+    cityHeader.innerHTML = resultFromServer.name;
+    humidity.innerHTML = 'Humidity levels at ' + resultFromServer.main.humidity + '%';
+
+
 }
